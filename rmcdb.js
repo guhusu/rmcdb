5a56d987d08d81a2e93ea2e111cb1e8a7d15e4c0
guhusu <guhusu@gmail.com>
2014-06-18 23:08:10 +0800

    Create rmcdb.js
    rmcdb js file

---
+++ rmcdb.js
@@ -0,0 +1,515 @@
+/* sha1 start */
+
+function sha1(str) {
+  //  discuss at: http://phpjs.org/functions/sha1/
+  // original by: Webtoolkit.info (http://www.webtoolkit.info/)
+  // improved by: Michael White (http://getsprink.com)
+  // improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  //    input by: Brett Zamir (http://brett-zamir.me)
+  //   example 1: sha1('Kevin van Zonneveld');
+  //   returns 1: '54916d2e62f65b3afa6e192e6a601cdbe5cb5897'
+
+  var rotate_left = function (n, s) {
+    var t4 = (n << s) | (n >>> (32 - s));
+    return t4;
+  };
+
+  /*var lsb_hex = function (val) {
+   // Not in use; needed?
+    var str="";
+    var i;
+    var vh;
+    var vl;
+
+    for ( i=0; i<=6; i+=2 ) {
+      vh = (val>>>(i*4+4))&0x0f;
+      vl = (val>>>(i*4))&0x0f;
+      str += vh.toString(16) + vl.toString(16);
+    }
+    return str;
+  };*/
+
+  var cvt_hex = function (val) {
+    var str = '';
+    var i;
+    var v;
+
+    for (i = 7; i >= 0; i--) {
+      v = (val >>> (i * 4)) & 0x0f;
+      str += v.toString(16);
+    }
+    return str;
+  };
+
+  var blockstart;
+  var i, j;
+  var W = new Array(80);
+  var H0 = 0x67452301;
+  var H1 = 0xEFCDAB89;
+  var H2 = 0x98BADCFE;
+  var H3 = 0x10325476;
+  var H4 = 0xC3D2E1F0;
+  var A, B, C, D, E;
+  var temp;
+
+  // utf8_encode
+  str = unescape(encodeURIComponent(str));
+  var str_len = str.length;
+
+  var word_array = [];
+  for (i = 0; i < str_len - 3; i += 4) {
+    j = str.charCodeAt(i) << 24 | str.charCodeAt(i + 1) << 16 | str.charCodeAt(i + 2) << 8 | str.charCodeAt(i + 3);
+    word_array.push(j);
+  }
+
+  switch (str_len % 4) {
+  case 0:
+    i = 0x080000000;
+    break;
+  case 1:
+    i = str.charCodeAt(str_len - 1) << 24 | 0x0800000;
+    break;
+  case 2:
+    i = str.charCodeAt(str_len - 2) << 24 | str.charCodeAt(str_len - 1) << 16 | 0x08000;
+    break;
+  case 3:
+    i = str.charCodeAt(str_len - 3) << 24 | str.charCodeAt(str_len - 2) << 16 | str.charCodeAt(str_len - 1) <<
+      8 | 0x80;
+    break;
+  }
+
+  word_array.push(i);
+
+  while ((word_array.length % 16) != 14) {
+    word_array.push(0);
+  }
+
+  word_array.push(str_len >>> 29);
+  word_array.push((str_len << 3) & 0x0ffffffff);
+
+  for (blockstart = 0; blockstart < word_array.length; blockstart += 16) {
+    for (i = 0; i < 16; i++) {
+      W[i] = word_array[blockstart + i];
+    }
+    for (i = 16; i <= 79; i++) {
+      W[i] = rotate_left(W[i - 3] ^ W[i - 8] ^ W[i - 14] ^ W[i - 16], 1);
+    }
+
+    A = H0;
+    B = H1;
+    C = H2;
+    D = H3;
+    E = H4;
+
+    for (i = 0; i <= 19; i++) {
+      temp = (rotate_left(A, 5) + ((B & C) | (~B & D)) + E + W[i] + 0x5A827999) & 0x0ffffffff;
+      E = D;
+      D = C;
+      C = rotate_left(B, 30);
+      B = A;
+      A = temp;
+    }
+
+    for (i = 20; i <= 39; i++) {
+      temp = (rotate_left(A, 5) + (B ^ C ^ D) + E + W[i] + 0x6ED9EBA1) & 0x0ffffffff;
+      E = D;
+      D = C;
+      C = rotate_left(B, 30);
+      B = A;
+      A = temp;
+    }
+
+    for (i = 40; i <= 59; i++) {
+      temp = (rotate_left(A, 5) + ((B & C) | (B & D) | (C & D)) + E + W[i] + 0x8F1BBCDC) & 0x0ffffffff;
+      E = D;
+      D = C;
+      C = rotate_left(B, 30);
+      B = A;
+      A = temp;
+    }
+
+    for (i = 60; i <= 79; i++) {
+      temp = (rotate_left(A, 5) + (B ^ C ^ D) + E + W[i] + 0xCA62C1D6) & 0x0ffffffff;
+      E = D;
+      D = C;
+      C = rotate_left(B, 30);
+      B = A;
+      A = temp;
+    }
+
+    H0 = (H0 + A) & 0x0ffffffff;
+    H1 = (H1 + B) & 0x0ffffffff;
+    H2 = (H2 + C) & 0x0ffffffff;
+    H3 = (H3 + D) & 0x0ffffffff;
+    H4 = (H4 + E) & 0x0ffffffff;
+  }
+
+  temp = cvt_hex(H0) + cvt_hex(H1) + cvt_hex(H2) + cvt_hex(H3) + cvt_hex(H4);
+  return temp.toLowerCase();
+}
+/* md5 start */
+function md5(str) {
+  //  discuss at: http://phpjs.org/functions/md5/
+  // original by: Webtoolkit.info (http://www.webtoolkit.info/)
+  // improved by: Michael White (http://getsprink.com)
+  // improved by: Jack
+  // improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  //    input by: Brett Zamir (http://brett-zamir.me)
+  // bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  //  depends on: utf8_encode
+  //   example 1: md5('Kevin van Zonneveld');
+  //   returns 1: '6e658d4bfcb59cc13f96c14450ac40b9'
+
+  var xl;
+
+  var rotateLeft = function (lValue, iShiftBits) {
+    return (lValue << iShiftBits) | (lValue >>> (32 - iShiftBits));
+  };
+
+  var addUnsigned = function (lX, lY) {
+    var lX4, lY4, lX8, lY8, lResult;
+    lX8 = (lX & 0x80000000);
+    lY8 = (lY & 0x80000000);
+    lX4 = (lX & 0x40000000);
+    lY4 = (lY & 0x40000000);
+    lResult = (lX & 0x3FFFFFFF) + (lY & 0x3FFFFFFF);
+    if (lX4 & lY4) {
+      return (lResult ^ 0x80000000 ^ lX8 ^ lY8);
+    }
+    if (lX4 | lY4) {
+      if (lResult & 0x40000000) {
+        return (lResult ^ 0xC0000000 ^ lX8 ^ lY8);
+      } else {
+        return (lResult ^ 0x40000000 ^ lX8 ^ lY8);
+      }
+    } else {
+      return (lResult ^ lX8 ^ lY8);
+    }
+  };
+
+  var _F = function (x, y, z) {
+    return (x & y) | ((~x) & z);
+  };
+  var _G = function (x, y, z) {
+    return (x & z) | (y & (~z));
+  };
+  var _H = function (x, y, z) {
+    return (x ^ y ^ z);
+  };
+  var _I = function (x, y, z) {
+    return (y ^ (x | (~z)));
+  };
+
+  var _FF = function (a, b, c, d, x, s, ac) {
+    a = addUnsigned(a, addUnsigned(addUnsigned(_F(b, c, d), x), ac));
+    return addUnsigned(rotateLeft(a, s), b);
+  };
+
+  var _GG = function (a, b, c, d, x, s, ac) {
+    a = addUnsigned(a, addUnsigned(addUnsigned(_G(b, c, d), x), ac));
+    return addUnsigned(rotateLeft(a, s), b);
+  };
+
+  var _HH = function (a, b, c, d, x, s, ac) {
+    a = addUnsigned(a, addUnsigned(addUnsigned(_H(b, c, d), x), ac));
+    return addUnsigned(rotateLeft(a, s), b);
+  };
+
+  var _II = function (a, b, c, d, x, s, ac) {
+    a = addUnsigned(a, addUnsigned(addUnsigned(_I(b, c, d), x), ac));
+    return addUnsigned(rotateLeft(a, s), b);
+  };
+
+  var convertToWordArray = function (str) {
+    var lWordCount;
+    var lMessageLength = str.length;
+    var lNumberOfWords_temp1 = lMessageLength + 8;
+    var lNumberOfWords_temp2 = (lNumberOfWords_temp1 - (lNumberOfWords_temp1 % 64)) / 64;
+    var lNumberOfWords = (lNumberOfWords_temp2 + 1) * 16;
+    var lWordArray = new Array(lNumberOfWords - 1);
+    var lBytePosition = 0;
+    var lByteCount = 0;
+    while (lByteCount < lMessageLength) {
+      lWordCount = (lByteCount - (lByteCount % 4)) / 4;
+      lBytePosition = (lByteCount % 4) * 8;
+      lWordArray[lWordCount] = (lWordArray[lWordCount] | (str.charCodeAt(lByteCount) << lBytePosition));
+      lByteCount++;
+    }
+    lWordCount = (lByteCount - (lByteCount % 4)) / 4;
+    lBytePosition = (lByteCount % 4) * 8;
+    lWordArray[lWordCount] = lWordArray[lWordCount] | (0x80 << lBytePosition);
+    lWordArray[lNumberOfWords - 2] = lMessageLength << 3;
+    lWordArray[lNumberOfWords - 1] = lMessageLength >>> 29;
+    return lWordArray;
+  };
+
+  var wordToHex = function (lValue) {
+    var wordToHexValue = '',
+      wordToHexValue_temp = '',
+      lByte, lCount;
+    for (lCount = 0; lCount <= 3; lCount++) {
+      lByte = (lValue >>> (lCount * 8)) & 255;
+      wordToHexValue_temp = '0' + lByte.toString(16);
+      wordToHexValue = wordToHexValue + wordToHexValue_temp.substr(wordToHexValue_temp.length - 2, 2);
+    }
+    return wordToHexValue;
+  };
+
+  var x = [],
+    k, AA, BB, CC, DD, a, b, c, d, S11 = 7,
+    S12 = 12,
+    S13 = 17,
+    S14 = 22,
+    S21 = 5,
+    S22 = 9,
+    S23 = 14,
+    S24 = 20,
+    S31 = 4,
+    S32 = 11,
+    S33 = 16,
+    S34 = 23,
+    S41 = 6,
+    S42 = 10,
+    S43 = 15,
+    S44 = 21;
+  str =unescape(encodeURIComponent(str));
+  //str = this.utf8_encode(str);
+  x = convertToWordArray(str);
+  a = 0x67452301;
+  b = 0xEFCDAB89;
+  c = 0x98BADCFE;
+  d = 0x10325476;
+
+  xl = x.length;
+  for (k = 0; k < xl; k += 16) {
+    AA = a;
+    BB = b;
+    CC = c;
+    DD = d;
+    a = _FF(a, b, c, d, x[k + 0], S11, 0xD76AA478);
+    d = _FF(d, a, b, c, x[k + 1], S12, 0xE8C7B756);
+    c = _FF(c, d, a, b, x[k + 2], S13, 0x242070DB);
+    b = _FF(b, c, d, a, x[k + 3], S14, 0xC1BDCEEE);
+    a = _FF(a, b, c, d, x[k + 4], S11, 0xF57C0FAF);
+    d = _FF(d, a, b, c, x[k + 5], S12, 0x4787C62A);
+    c = _FF(c, d, a, b, x[k + 6], S13, 0xA8304613);
+    b = _FF(b, c, d, a, x[k + 7], S14, 0xFD469501);
+    a = _FF(a, b, c, d, x[k + 8], S11, 0x698098D8);
+    d = _FF(d, a, b, c, x[k + 9], S12, 0x8B44F7AF);
+    c = _FF(c, d, a, b, x[k + 10], S13, 0xFFFF5BB1);
+    b = _FF(b, c, d, a, x[k + 11], S14, 0x895CD7BE);
+    a = _FF(a, b, c, d, x[k + 12], S11, 0x6B901122);
+    d = _FF(d, a, b, c, x[k + 13], S12, 0xFD987193);
+    c = _FF(c, d, a, b, x[k + 14], S13, 0xA679438E);
+    b = _FF(b, c, d, a, x[k + 15], S14, 0x49B40821);
+    a = _GG(a, b, c, d, x[k + 1], S21, 0xF61E2562);
+    d = _GG(d, a, b, c, x[k + 6], S22, 0xC040B340);
+    c = _GG(c, d, a, b, x[k + 11], S23, 0x265E5A51);
+    b = _GG(b, c, d, a, x[k + 0], S24, 0xE9B6C7AA);
+    a = _GG(a, b, c, d, x[k + 5], S21, 0xD62F105D);
+    d = _GG(d, a, b, c, x[k + 10], S22, 0x2441453);
+    c = _GG(c, d, a, b, x[k + 15], S23, 0xD8A1E681);
+    b = _GG(b, c, d, a, x[k + 4], S24, 0xE7D3FBC8);
+    a = _GG(a, b, c, d, x[k + 9], S21, 0x21E1CDE6);
+    d = _GG(d, a, b, c, x[k + 14], S22, 0xC33707D6);
+    c = _GG(c, d, a, b, x[k + 3], S23, 0xF4D50D87);
+    b = _GG(b, c, d, a, x[k + 8], S24, 0x455A14ED);
+    a = _GG(a, b, c, d, x[k + 13], S21, 0xA9E3E905);
+    d = _GG(d, a, b, c, x[k + 2], S22, 0xFCEFA3F8);
+    c = _GG(c, d, a, b, x[k + 7], S23, 0x676F02D9);
+    b = _GG(b, c, d, a, x[k + 12], S24, 0x8D2A4C8A);
+    a = _HH(a, b, c, d, x[k + 5], S31, 0xFFFA3942);
+    d = _HH(d, a, b, c, x[k + 8], S32, 0x8771F681);
+    c = _HH(c, d, a, b, x[k + 11], S33, 0x6D9D6122);
+    b = _HH(b, c, d, a, x[k + 14], S34, 0xFDE5380C);
+    a = _HH(a, b, c, d, x[k + 1], S31, 0xA4BEEA44);
+    d = _HH(d, a, b, c, x[k + 4], S32, 0x4BDECFA9);
+    c = _HH(c, d, a, b, x[k + 7], S33, 0xF6BB4B60);
+    b = _HH(b, c, d, a, x[k + 10], S34, 0xBEBFBC70);
+    a = _HH(a, b, c, d, x[k + 13], S31, 0x289B7EC6);
+    d = _HH(d, a, b, c, x[k + 0], S32, 0xEAA127FA);
+    c = _HH(c, d, a, b, x[k + 3], S33, 0xD4EF3085);
+    b = _HH(b, c, d, a, x[k + 6], S34, 0x4881D05);
+    a = _HH(a, b, c, d, x[k + 9], S31, 0xD9D4D039);
+    d = _HH(d, a, b, c, x[k + 12], S32, 0xE6DB99E5);
+    c = _HH(c, d, a, b, x[k + 15], S33, 0x1FA27CF8);
+    b = _HH(b, c, d, a, x[k + 2], S34, 0xC4AC5665);
+    a = _II(a, b, c, d, x[k + 0], S41, 0xF4292244);
+    d = _II(d, a, b, c, x[k + 7], S42, 0x432AFF97);
+    c = _II(c, d, a, b, x[k + 14], S43, 0xAB9423A7);
+    b = _II(b, c, d, a, x[k + 5], S44, 0xFC93A039);
+    a = _II(a, b, c, d, x[k + 12], S41, 0x655B59C3);
+    d = _II(d, a, b, c, x[k + 3], S42, 0x8F0CCC92);
+    c = _II(c, d, a, b, x[k + 10], S43, 0xFFEFF47D);
+    b = _II(b, c, d, a, x[k + 1], S44, 0x85845DD1);
+    a = _II(a, b, c, d, x[k + 8], S41, 0x6FA87E4F);
+    d = _II(d, a, b, c, x[k + 15], S42, 0xFE2CE6E0);
+    c = _II(c, d, a, b, x[k + 6], S43, 0xA3014314);
+    b = _II(b, c, d, a, x[k + 13], S44, 0x4E0811A1);
+    a = _II(a, b, c, d, x[k + 4], S41, 0xF7537E82);
+    d = _II(d, a, b, c, x[k + 11], S42, 0xBD3AF235);
+    c = _II(c, d, a, b, x[k + 2], S43, 0x2AD7D2BB);
+    b = _II(b, c, d, a, x[k + 9], S44, 0xEB86D391);
+    a = addUnsigned(a, AA);
+    b = addUnsigned(b, BB);
+    c = addUnsigned(c, CC);
+    d = addUnsigned(d, DD);
+  }
+
+  var temp = wordToHex(a) + wordToHex(b) + wordToHex(c) + wordToHex(d);
+
+  return temp.toLowerCase();
+} 
+/* md5 end*/
+/* sha1 end*/
+var RMCDB={
+				_NAME:'RDB',//localstorage name
+				_TABLE:'',//目前使用的table
+				_TABLE_NUMS:0,//資料表數量
+				_DATA:'',//select 後的資料
+				_DB:{},//目前所有資料
+				_IS_INIT:false,//是否已經初始化
+				_ERROE_TYPE:1,//1.return 2.alert
+				_ERROR:'',
+				_AUTO_TYPE:3,//自動編碼id 1.sha1 2.md5 3.沒有編碼
+				//初始化
+				init:function(s){
+					if(localStorage[this._NAME]==undefined || localStorage[this._NAME]==null || localStorage[this._NAME]=='') localStorage[this._NAME]='{}';
+					else{
+						this._DB=JSON.parse(localStorage[this._NAME]);//alert(localStorage[this._NAME]+' -- '+this._DB);
+					}
+
+					//設定
+					if(s!=undefined){
+						if(s.error!=undefined){
+							this.setErrorType(s.error);
+						}
+						if(s.auto!=undefined){
+							this.setAutoId(s.auto);
+						}
+					}
+					
+					this._IS_INIT=true;
+				},
+				//回寫儲存資料
+				save:function(){
+					localStorage[this._NAME]=JSON.stringify(this._DB);
+				},
+				//設定autoid
+				setAutoId:function(n){
+					this._AUTO_TYPE=n;
+				},
+				//取得autoid id
+				getAutoId:function(name){
+					var id=this._DB[name+'_autoid'];
+					switch(this._AUTO_TYPE)
+					{
+						case 1:
+							id=sha1(id);
+							break;
+						case 2:
+							id=md5(id);
+							break;
+						default:
+							break;
+					}
+
+					return id;
+				},
+				//設定error type
+				setErrorType:function(n){
+					this._ERROR_TYPE=n;
+				},
+				//顯示錯誤
+				showError:function(str){
+					this._ERROR=str;
+					switch(this._ERROR_TYPE)
+					{
+						case 1:
+							return str;
+							break;
+						case 2:
+							alert(str);
+							break;
+						default:break;
+					}
+				},
+				//建立資料表
+				createTable:function(name){
+					name=name.toLowerCase();//alert(this._DB[name]);
+					if(this._DB[name]==undefined){
+						this._DB[name]=[];
+						this._DB[name+'_autoid']=1;
+						this.save();
+					}else{
+						this.showError('資料表已存在');
+					}//alert(this._DB[name]);
+				},
+				//清空資料表
+				truncate:function(name){
+					name=name.toLowerCase();
+					if(this._DB[name]!=undefined){
+						this._DB[name]=[];
+						this._DB[name+'_autoid']=1;
+						this.save();
+					}else{
+						this.showError('資料表不存在');
+					}
+				},
+				//刪除資料表
+				dropTable:function(name){
+					name=name.toLowerCase();
+					if(this._DB[name]!=undefined){
+						this._DB[name]=null;
+						this._DB[name+'_autoid']=null;
+						delete this._DB[name];
+						delete this._DB[name+'_autoid'];
+						this.save();
+					}else{
+						this.showError('資料表不存在');
+					}
+				},
+				//新增資料表
+				insert:function(name,da){//alert(RMCDB._DB+' -- '+this._DB['user']+' == '+this._DB[name]+' -- '+name);
+					name=name.toLowerCase();
+					if(this._DB[name]!=undefined){
+						da.rid=this.getAutoId(name);
+                        this._DB[name].push(da);
+                        this._DB[name+'_autoid'] +=1;
+                        this.save();
+					}else{
+						this.showError('無此資料表');
+					}
+				},
+				//更新資料表
+				update:function(name,da,arg){
+					var tmp;
+					var up
+					name=name.toLowerCase();
+					if(this._DB[name]!=undefined){
+						for(var i in this._DB[name]){
+							tmp=this._DB[name][i];
+							up=true;
+							for(var j in arg){
+								if(tmp[j]==undefined || tmp[j]!=arg[j]) up=false;
+								if(!up) break;
+							}
+							//update
+							if(up){
+								for(var j in da){
+									tmp[j]=da[j];
+								}
+							}
+							/*for(var j in tmp){
+								up=false;
+								for(var k in arg){
+
+								}
+							}*/
+						}
+                        this.save();
+					}else{
+						this.showError('無此資料表');
+					}
+				}
+};
